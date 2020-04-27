22cb4b217b3e331a5b367001ab644437a73dfadd
Kleight <martinwongso234@gmail.com>
2020-04-27 14:39:03 +0700

    Update

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,8 +2,8 @@
   <h2 class="display-4 text-center"> Welcome to <span class="fs-home-logo">FoodShala</span> <br> Meals That Matter.</h2>
   <?php if (!$this->session->userdata('logged_in')) : ?>
   <div class= "fs-home-main-buttons">
-    <button class="btn btn-primary fs-home-main-button" type="submit" href="<?php echo(base_url().'users/register'); ?>" role="button">Register</button>
-    <button class="btn btn-success fs-home-main-button" type="submit" href="<?php echo(base_url().'users/login'); ?>" role="button">Login</button>
+    <a class="btn btn-primary fs-home-main-button" type="submit" href="<?php echo(base_url().'users/register'); ?>" role="button">Register</a>
+    <a class="btn btn-success fs-home-main-button" type="submit" href="<?php echo(base_url().'users/login'); ?>" role="button">Login</a>
   </div>
   <?php endif; ?>
 </div>
