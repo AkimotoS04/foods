465e138a1518ff1c1bff7b3b37d6a46c656dd3b4
AkimotoS04 <47805144+AkimotoS04@users.noreply.github.com>
2020-04-29 12:09:18 +0700

    Test push
    Ubah jarak button register-login

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,26 +1,32 @@
 <?php echo $navbar; ?>
 <div class="jumbotron w3-container w3-center w3-animate-zoom" style="color:white; background-color: rgba(255, 255, 255, 0); padding-top: 10%;">
   <h2 class="display-4 text-center"> Welcome to <span class="monospace">FoodShala</span> <br> Meals That Matter.</h2>
-  <br/><br/>  
+  <br/><br/>
   <?php if (!$this->session->userdata('logged_in')) : ?>
     <!--Button-->
-    <a class="my-super-cool-btn"  type="submit" href="<?php echo(base_url().'users/register'); ?>" role="button"> 
-    <div class="dots-container" >
-      <div class="dot"></div>
-      <div class="dot"></div>
-      <div class="dot"></div>
-      <div class="dot"></div>
-    </div>
-    <span style="color:white">Register</span>
-  </a>
+    <span class="m-sm-3">
+      <a class="my-super-cool-btn"  type="submit" href="<?php echo(base_url().'users/register'); ?>" role="button">
+        <div class="dots-container" >
+          <div class="dot"></div>
+          <div class="dot"></div>
+          <div class="dot"></div>
+          <div class="dot"></div>
+        </div>
+        <span style="color:white">Register</span>
+      </a>
+    </span>
+
+    <span class="m-sm-3">
     <a class="my-super-cool-btn" type="submit" href="<?php echo(base_url().'users/login'); ?>" role="button">
-    <div class="dots-container" >
-      <div class="dot"></div>
-      <div class="dot"></div>
-      <div class="dot"></div>
-      <div class="dot"></div>
-    </div>
-    <span style="color:white">Login</span>
-  </a>
+      <div class="dots-container" >
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+      </div>
+      <span style="color:white">Login</span>
+    </a>
+  </span>
+
   <?php endif; ?>
 </div>
