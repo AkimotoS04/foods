25ca995035397955b26ff9bd7de02e57e8b1aa18
Kleight <martinwongso234@gmail.com>
2020-05-02 12:24:43 +0700

    view

--- application/views/about/index.php
+++ application/views/about/index.php
@@ -1,8 +1,8 @@
-<div class=" w3-container w3-center w3-animate-zoom" style="margin-top:140px;color:white">
+<div class=" w3-container w3-center w3-animate-zoom py-5" style="margin-top:140px; color:white; background-color: rgba(255, 255, 255, 0);">
 <h2><?= $title ?></h2>
 <br>
-<h5><span class="fs-home-logo" style="font-size:40px">Agnesia</span>  </h5>
-<h5><span class="fs-home-logo" style="font-size:40px">Louis Jonathan</span>  </h5>
-<h5><span class="fs-home-logo" style="font-size:40px">Martin Wongso</span>  </h5>
-<h5><span class="fs-home-logo" style="font-size:40px">Yonathan Christian</span>  </h5>
+<h5><span class="fs-home-logo" style="font-size:40px">Agnesia (00000027731)</span>  </h5>
+<h5><span class="fs-home-logo" style="font-size:40px">Louis Jonathan (00000027658)</span>  </h5>
+<h5><span class="fs-home-logo" style="font-size:40px">Martin Wongso (00000027553)</span>  </h5>
+<h5><span class="fs-home-logo" style="font-size:40px">Yonathan Christian (00000027410)</span>  </h5>
 </div>
