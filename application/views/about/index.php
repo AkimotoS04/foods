1c6adb15f9c937983272355baabf3748041f68f3
AkimotoS04 <47805144+AkimotoS04@users.noreply.github.com>
2020-05-19 21:51:18 +0700

    ayyy
    buttons + About page:v

--- application/views/about/index.php
+++ application/views/about/index.php
@@ -1,8 +1,11 @@
 <div class="container text-center w3-animate-zoom py-5" style="margin-top:140px; color:white; background-color: rgba(255, 255, 255, 0);">
-<h1 class="text-center fs-home-logo"><?= $title ?></h1>
+<span><h2 class="text-center fs-home-logo"><?= $title ?></h2></span>
 <hr>
-<h5><span class="fs-home-logo" style="font-size:40px">Agnesia (00000027731)</span>  </h5>
-<h5><span class="fs-home-logo" style="font-size:40px">Louis Jonathan (00000027658)</span>  </h5>
-<h5><span class="fs-home-logo" style="font-size:40px">Martin Wongso (00000027553)</span>  </h5>
-<h5><span class="fs-home-logo" style="font-size:40px">Yonathan Christian (00000027410)</span>  </h5>
+<h4><span>This web is developed to complete the Final Project of Web Programming Class of <br>Multimedia Nusantara University's Informatics Students</span></h4>
+<br>
+<h3><span>10<small>th</small> Group : </span></h4>
+<h3><span class="fs-home-logo" style="font-size:40px">Agnesia (00000027731)</span></h3>
+<h3><span class="fs-home-logo" style="font-size:40px">Louis Jonathan (00000027658)</span></h3>
+<h3><span class="fs-home-logo" style="font-size:40px">Martin Wongso (00000027553)</span></h3>
+<h3><span class="fs-home-logo" style="font-size:40px">Yonathan Christian (00000027410)</span></h3>
 </div>
--- application/views/foods/view_history.php
+++ application/views/foods/view_history.php
@@ -4,23 +4,37 @@
 <?php foreach ($foods as $key => $food) : ?>
   <h4><?php echo $jumlah[$key]; ?> x <?php echo $fname[$key]; ?> -
   <small><?php echo $rname[$key]; ?> </small>
-  <span>Give Rating</span>
+  <br/>
+  <div class="my-2">
+    <small>Total Harga : <?php echo("Rp. ".$price[$key] * $jumlah[$key].",-"); ?></small>
+  </div>
+  <div>
   <form method='POST' action='<?php echo base_url('../foods/foods/rating'); ?>' enctype='multipart/form-data'>
-					<div class="qty">
-
-              <input class="count" type="number" value="1" min="1" max="5" name="rating">
-
-          </div>
-					<hr>
-          <br>
-          <input type='hidden' name='id' value="<?php echo $food['id'] ?>">
-					<button type='submit' name='submit' class='btn btn-success fs-food-page'>Give Rating</button>
-				</form>
-  <br>
-  <br>
-
-  <small><?php echo("Rp. ".$price[$key] * $jumlah[$key].",-"); ?></small>
-  <br><hr>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text border-dark  bg-dark text-white" id="basic-addon3"><b>Rating</b></span>
+      </div>
+      <div class="qty mx-3">
+        <span class="minus bg-dark qty-minus">-</span>
+        <input class="count pl-3 border-dark rounded" type="number" style="width:50px;" value="1" min="1" max="5" name="rating" readonly>
+        <span class="minus bg-dark qty-plus">+</span>
+      </div>
+      <div class="input-group-prepend">
+        <input type='hidden' name='id' value="<?php echo $food['id'] ?>">
+        <button type='submit' name='submit' class='btn btn-success font-weight-bold rounded fs-food-page'>Give Rating</button>
+      </div>
+    </div>
+		<hr>
+	</form>
+</div>
 </h4>
 <?php endforeach; ?>
 </div>
+<script>
+    $(document).on('click', '.qty-plus', function () {
+		$(this).prev().val(+$(this).prev().val() + 1);
+	});
+	$(document).on('click', '.qty-minus', function () {
+		if ($(this).next().val() > 1) $(this).next().val(+$(this).next().val() - 1);
+	});
+</script>
