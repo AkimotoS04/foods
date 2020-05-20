40b8362a58644871728753c99fdbbb212a8b124c
Kleight <martinwongso234@gmail.com>
2020-05-20 22:46:43 +0700

    index

--- application/views/foods/index.php
+++ application/views/foods/index.php
@@ -74,27 +74,6 @@
 <?php endforeach; ?>
 </div>
 
-<div class="modal fade bd-example-modal-lg" style="width: 100%;" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Sales Statistic</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-		<div class="container">
-			<div id="chartContainer" style="height: 370px; width: auto;"></div>
-		</div>	
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
 <script>
     $(document).on('click', '.qty-plus', function () {
 		$(this).prev().val(+$(this).prev().val() + 1);
