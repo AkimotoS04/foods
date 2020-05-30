404f837a5b94afef91501d73ad585d0d2d37c6b5
Kleight <martinwongso234@gmail.com>
2020-05-30 21:37:32 +0700

    view order (penjual)

--- application/controllers/Foods.php
+++ application/controllers/Foods.php
@@ -513,6 +513,11 @@ class Foods extends CI_Controller
                     $name = $this->food_model->get_jumlah($data['orders'][$x]['id']);
                     array_push($data['jumlah'], $name);
                 }
+                $data['rating'] = [];
+                for ($x = 0; $x <= count($data['orders']) - 1; $x++) {
+                    $name = $this->food_model->get_rating($data['orders'][$x]['id']);
+                    array_push($data['rating'], $name);
+                }
 
                 $this->load->view('templates/header', $data);
                 $this->load->view('foods/view_orders', $data);
--- application/views/foods/view_orders.php
+++ application/views/foods/view_orders.php
@@ -7,6 +7,25 @@
 <?php else :?>
   <h4> <?php echo $uname[$key]; ?></h4><h4>
   <?php echo $jumlah[$key]; ?> x <?php echo $food[$key]; ?>
+  <br/>
+  <?php
+    if($rating[$key]!=0)
+    {
+      for($x=0;$x<floor($rating[$key]);$x++){
+        echo'<span class="fa fa-star checked"></span>';
+        echo" ";
+      }
+      for($j=$x;$j<5;$j++){
+        echo'<span class="fa fa-star"></span>';
+        echo" ";
+      }
+    }else{
+      for($x=0;$x<5;$x++){
+        echo'<span class="fa fa-star checked"></span>';
+        echo" ";
+      }
+    }
+  ?>
   </h4>
   <h6><?php echo $email[$key]; ?></h6>
 <?php endif;?>
