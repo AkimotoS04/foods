a27eee7bd3e5d6b4d72110f7a702e3f2c95ee98e
Kleight <martinwongso234@gmail.com>
2020-06-01 12:48:31 +0700

    foods.php

--- application/controllers/Foods.php
+++ application/controllers/Foods.php
@@ -676,6 +676,13 @@ class Foods extends CI_Controller
             $name = $this->food_model->get_rating($data['foods'][$x]['id']);
             array_push($data['rating'], $name);
         }
+        if ($this->session->userdata('user_type') != null) {
+            if ($this->session->userdata('user_type') == 0 && strcmp($this->session->userdata('email'),'superadmin@gmail.com') == 0) {
+
+                $data['new'] = $this->food_model->get_request();
+
+            }
+        }
 
         $this->load->view('templates/header', $data);
         $this->load->view('foods/view_menu', $data);
